6183ba305d15e7280cb24d34b4d511317f4bbbcf
lestrellaap@uanl.edu.mx <lestrellaap@uanl.edu.mx>
2023-10-09 09:18:40 -0600

    Atualizar Git

--- 06y07_Actividad_regresion.R
+++ 06y07_Actividad_regresion.R
@@ -38,16 +38,40 @@ plot(erupciones$waiting, erupciones$eruptions,
      col = "red",
      pch = 19,
      main = "Leobardo Estrella")
-
+abline(erump.lm,
+       col= "blue") # sirve para despegar la linea de tendencia central 
 cor.test(erupciones$eruptions, erupciones$waiting)
 
 text(50,4.5, "r = 0.9008112***")
 
+# Leobardo Esstrella 
+# 09/10/2023
 
+erump.lm <-lm(erupciones$eruptions ~ erupciones$waiting)
 
+erump.lm #obtener el valor d alfa y beta 
 
+summary(erump.lm) # obtener la significancia de alfa y beta 
+ #valor de r ajustado quiere decir que se puede transformar en %
 
+erupciones$yprima <- -1.874 + 0.075* erupciones$waiting
+erupciones$estimados <- erump.lm$fitted.values #valores ajustados 
+range(erupciones$waiting)
 
+erup <- c(80, 43, 45, 53, 61, 95)
+y.pr <- -1.874 + 0.075*erup
+erup
+y.pr
 
+# Aplicar la forma de manera individual 
+#alfa + beta * valor de x 
 
+-1.874 + 0.075* 80
+-1.874 + 0.075* 43
+-1.874 + 0.075* 45
+-1.874 + 0.075* 53
+-1.874 + 0.075* 61
+-1.874 + 0.075* 95
 
+# el rango de los valores de x son mi ventana de prediccion de yprima ## range(erupciones$waiting) 
+#
